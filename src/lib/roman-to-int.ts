570b87cea5954da7f3e2c4ab5495f97188e35d22
Peter Huang <peter.huang@neofinancial.com>
2022-10-19 13:46:42 -0600

    roman to int

---
+++ src/lib/roman-to-int.ts
@@ -0,0 +1,22 @@
+const romanToInt = (s: string): number => {
+  const romanMap: { [key: string]: number } = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (romanMap[s[i]] < romanMap[s[i + 1]]) {
+      result -= romanMap[s[i]];
+    } else {
+      result += romanMap[s[i]];
+    }
+  }
+  return result;
+};
+
+export { romanToInt };
