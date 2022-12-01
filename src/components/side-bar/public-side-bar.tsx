3195a50aa9372bae9a6ec6a5ec19a200a94e8c9b
phuan516 <phuan516@mtroyal.ca>
2022-12-01 15:18:28 -0700

    remove login from public nav

--- src/components/side-bar/public-side-bar.tsx
+++ src/components/side-bar/public-side-bar.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { CgProfile } from "react-icons/cg";
 
 import PublicSideBarIcons from "./public-side-bar-icons";
-import SideBarIcon from "./side-bar-icon";
 import GithubStatus from "./github-status";
 
 const PublicSideBar = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg justify-between">
       <PublicSideBarIcons />
-      <div>
-        <GithubStatus />
-        <SideBarIcon icon={<CgProfile size="18" />} link="/profile" />
-      </div>
+      <GithubStatus />
     </div>
   );
 };
